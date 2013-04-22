d4eaf33f5ae8f95829af839c665ecb041ac6a804
Jason Anderson <jason@jasonjanderson.com>
2013-04-21 22:19:17 -0500

    Initial Commit

---
+++ CacheStore/Cache.cs
@@ -0,0 +1,74 @@
+﻿#region "Imports"
+
+using System;
+using System.Collections.Concurrent;
+using System.Runtime.Caching;
+
+#endregion
+
+namespace CacheStore
+{
+    public static class Cache
+    {
+        private const string DefaultCacheName = "default";
+
+        public static ConcurrentDictionary<string, MemoryCache> _caches;
+
+        #region "Constructors"
+
+        static Cache()
+        {
+            _caches = new ConcurrentDictionary<string, MemoryCache>();
+        }
+
+        #endregion
+
+        public static T Get<T>(CacheKey key)
+        {
+            object o = GetCache(key.CacheName).Get(key.GenerateKey());
+            if (o is T)
+            {
+                return (T)o;
+            }
+            else
+            {
+                return default(T);
+            }
+
+        }
+
+        public static bool TryGetValue<T>(CacheKey key, out T value)
+        {
+            object o = GetCache(key.CacheName).Get(key.GenerateKey());
+            if (o is T)
+            {
+                value = (T)o;
+                return true;
+            }
+            value = default(T);
+            return false;
+        }
+
+        public static void Set<T>(CacheKey key, T value)
+        {
+            CacheItemPolicy policy = new CacheItemPolicy();
+            if (key.AbsoluteExpiration != DateTimeOffset.MaxValue) policy.AbsoluteExpiration = key.AbsoluteExpiration;
+            if (key.SlidingExpiration != TimeSpan.MaxValue) policy.SlidingExpiration = key.SlidingExpiration;
+            GetCache(key.CacheName).Set(key.GenerateKey(), value, policy);
+
+        }
+
+        private static MemoryCache GetCache(string cacheName)
+        {
+            MemoryCache c = null;
+            if (!_caches.TryGetValue(cacheName, out c))
+            {
+                // Use the default cache manager if a manger isn't defined
+                c = (cacheName == DefaultCacheName || cacheName == string.Empty) ? MemoryCache.Default : new MemoryCache(cacheName);
+                _caches[cacheName] = c;
+            }
+            return c;
+        }
+
+    }
+}
---
+++ CacheStore/CacheKey.cs
@@ -0,0 +1,54 @@
+﻿#region "Imports"
+
+using System;
+
+#endregion
+
+namespace CacheStore
+{
+    public abstract class CacheKey
+    {
+        private string _cacheName;
+        private DateTimeOffset _absoluteExpiration;
+        private TimeSpan _slidingExpiration;
+
+        #region "Properties"
+
+        public string CacheName
+        {
+            get { return _cacheName; }
+            set { _cacheName = value; }
+        }
+
+        public DateTimeOffset AbsoluteExpiration
+        {
+            get { return _absoluteExpiration; }
+            set { _absoluteExpiration = value; }
+        }
+
+        public TimeSpan SlidingExpiration
+        {
+            get { return _slidingExpiration; }
+            set { _slidingExpiration = value; }
+        }
+
+        #endregion
+
+        #region "Constructors"
+
+        public CacheKey()
+        {
+            _absoluteExpiration = DateTimeOffset.MaxValue;
+            _slidingExpiration = TimeSpan.MaxValue;
+        }
+
+        public CacheKey(string cacheName) : this()
+        {
+            _cacheName = cacheName;
+        }
+
+        #endregion
+
+        public abstract string GenerateKey();
+    }
+}
